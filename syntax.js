1b857b4b883f260521dc766c9cb92a54b71cbfbf
DigbijoySonowal <sonowaldigbijoy51@gmail.com>
2024-02-02 23:11:03 +0530

    more js

---
+++ syntax.js
@@ -0,0 +1,9 @@
+var x, y;
+x = 10;
+y = 20;
+var z = x + y;
+
+let fname, lname;
+fname = "Digbijoy";
+lname = "Sonowal";
+let fullname = fname +" "+ lname;
---
+++ variables.js
@@ -0,0 +1,11 @@
+const x = 5;
+const y = 6;
+let z = x + y;
+console.log(z);
+
+const name = "Digbijoy Sonowal";
+let roll_no = 7019;
+const sem = "6th"
+branch = "CSE";
+console.log(name + "\n" + roll_no + "\n" + branch);
+document.write(name+" "+roll_no+" "+sem+" sem "+branch);

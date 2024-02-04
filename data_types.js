4d332d78966bab0cdd466dd2df666eca713769d3
DigbijoySonowal <sonowaldigbijoy51@gmail.com>
2024-02-04 09:16:04 +0530

    Data Types

---
+++ data_types.js
@@ -0,0 +1,41 @@
+/*JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+
+The Object Datatype
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date*/
+
+//Numbers:
+let length = 20;
+let breadth = 30;
+
+//Strings:
+let color = "blue";
+let animal = "cow";
+
+//Boolean:
+let x = true;
+let y = false;
+
+//Object:
+const person = { first_name: "Digbijoy", last_name: "Sonowal" };
+
+//Array Object:
+const cars = ["BMW", "Bugatti", "Lamborgini"];
+
+//Date Object:
+const date = new Date("2024-02-04");
+
+let car; //undefined
+
+let log = ""; //Empty String, The value is "", the typeof is "string"
